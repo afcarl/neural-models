b26cc4471bfcf299022db1ee02fdeefb557cff85
Léonard Blier <leonard.blier@gmail.com>
2016-03-02 14:26:03 +0100

    delete the cv2 import

--- convnets.py
+++ convnets.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
-import cv2, numpy as np
+import numpy as np
 
 from scipy.misc import imread, imresize, imsave
 
