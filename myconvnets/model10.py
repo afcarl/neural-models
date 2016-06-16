6c1051dc8e3d0ed86f90b954441b2f90093b647b
Leonard Blier <leonard.blier@gmail.com>
2016-06-16 09:32:45 +0000

    Add the models

---
+++ myconvnets/model10.py
@@ -0,0 +1,39 @@
+from keras.optimizers import SGD, Adam
+from keras.regularizers import l2
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Activation
+
+from convnetskeras. convnets import convnet
+
+trainable_layers = ["dense_1", "conv5_1", "conv5_2",  "conv5_3",
+                    "conv4_1", "conv4_2",  "conv4_3"]
+
+
+
+
+vgg16 = convnet('vgg_16',
+                  weights_path='/srv/data/convnetsweights/vgg16_weights.h5',
+                  trainable=["None"])
+
+for l in vgg16.layers:
+    if l.name in trainable_layers:
+        pass
+    else:
+        l.trainable = False
+        
+
+input = vgg16.input
+img_representation = vgg16.get_layer("dense_1").output
+
+classifier = Dense(2,name='classifier',
+                   W_regularizer=l2(0.01))(img_representation)
+classifier = Activation("softmax", name="softmax")(classifier)
+
+model = Model(input=input,output=classifier)
+
+opt = SGD(lr=.1, decay=1.e-6, momentum=0.9, nesterov=False)
+#opt = Adam()
+
+
+
+model.compile(optimizer=opt, loss='categorical_crossentropy',metrics=["accuracy"])
---
+++ myconvnets/model9.py
@@ -0,0 +1,38 @@
+from keras.optimizers import SGD
+from keras.regularizers import l2
+from keras.models import Model
+from keras.layers import Dense, Dropout, Input, Activation
+
+from convnetskeras. convnets import convnet
+
+trainable_layers = ["conv5_1", "conv5_2",  "conv5_3"]
+
+
+
+
+vgg16 = convnet('vgg_16',
+                  weights_path='/srv/data/convnetsweights/vgg16_weights.h5',
+                  trainable=["None"])
+
+for l in vgg16.layers:
+    if l.name in trainable_layers:
+        pass
+    else:
+        l.trainable = False
+        
+
+input = vgg16.input
+img_representation = vgg16.get_layer("flatten").output
+
+classifier = Dense(2,name='classifier',
+                   W_regularizer=l2(0.001))(img_representation)
+classifier = Activation("softmax", name="softmax")(classifier)
+
+model = Model(input=input,output=classifier)
+
+sgd = SGD(lr=.1, decay=1.e-6, momentum=0.9, nesterov=False)
+
+
+
+model.compile(optimizer=sgd, loss='categorical_crossentropy',metrics=["accuracy"])
+
